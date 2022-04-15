408fd94ed7944f8528b096a93f2b4cd46f0b9884
lnuvy <hangeulgod@naver.com>
2022-04-16 01:24:05 +0900

    브랜치 분기 테스트 커밋

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   const dispatch = useDispatch();
   const isLogin = useSelector((state) => state.user.isLogin);
 
+  // 테스트
   // 로컬 스토리지에 토큰이 있는지 검사 후, 있다면 해당 토큰으로 user정보를 반환해옵니다
   useEffect(() => {
     if (!isLogin) {
